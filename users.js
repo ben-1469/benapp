d87f9e22f3d8ee9c84437f856b011ae7266e4900
Benjamin Ding <87156848+ben-1469@users.noreply.github.com>
2021-12-14 17:52:17 -0500

    Update users.js

--- users.js
+++ users.js
@@ -1,12 +1,33 @@
-var express = require('express');
-var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET home page. */
-router.get('/:id', function(req, res, next) {
-  res.id = req.params.id;
-  //const result = users.find((v) => v.id === id);
+const connectionString =
+  'mongodb+srv://admin:se-3@cluster0.d9pob.mongodb.net/test';
 
-  //res.json(result);
-});
+let isConnected;
 
-module.exports = router;
+const connectToDatabase = () => {
+  if (isConnected) {
+    console.log('=> using existing database connection');
+    return Promise.resolve();
+  }
+  console.log('=> using new database connection');
+
+  if (connectionString) {
+    return mongoose
+      .connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then((db) => {
+        isConnected = db.connections[0].readyState;
+        console.log('Connection Success!');
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  } else {
+    console.log('MongoDB url not set in environment variable');
+  }
+};
+
+module.exports = connectToDatabase;
